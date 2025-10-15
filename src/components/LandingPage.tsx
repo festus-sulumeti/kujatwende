edde8020b80eb8247f26d439ad4653d1fd5711a9
festus-sulumeti <sulumetifestus@outlook.com>
2025-10-15 13:28:44 +0300

    fix: importation

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -6,6 +6,7 @@ import { Menu, Rocket, X } from 'lucide-react'
 import { Button } from '../components/ui/button'
 // import Image from 'next/image'
 import Safari from '../assets/safari.jpeg'
+
 import FAQ from './FAQ'
 
 import Footer from './Footer'
